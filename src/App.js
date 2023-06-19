5eea25d434722f9d8e491f3d3727a39aee2b0a5e
Filip <filip.tokar@seznam.cz>
2023-06-19 21:17:51 +0200

    casting shadows on planeGeometry object from the directional light

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import './App.scss';
-import React from 'react';
-import { Canvas } from 'react-three-fiber';
-import BlueprintMesh from './Components/BlueprintMesh';
+import "./App.scss";
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import BlueprintMesh from "./Components/BlueprintMesh";
 
 function App() {
-  
   return (
     <>
-    <Canvas legacy={true} camera={{position: [-5,2, 10], fov:60}}> 
-      <ambientLight intensity={0.3}/>
-      <BlueprintMesh position={[0,1,0]}/>
-      <BlueprintMesh position={[-2,1,-5]}/>
-      <BlueprintMesh position={[5,1,-2]}/>
-    </Canvas>
+      <Canvas legacy={true} camera={{ position: [-5, 2, 10], fov: 60 }} shadows>
+        <ambientLight intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize={[1024, 1024]}
+          shadow-radius={8}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <group>
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+          >
+            <planeGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+            
+          </mesh>
+        </group>
+        <BlueprintMesh position={[0, 1, 0]} args={[3, 2, 1]} color="blue" />
+        <BlueprintMesh position={[-2, 1, -5]} color="red" />
+        <BlueprintMesh position={[5, 1, -2]} color="hotpink" />
+      </Canvas>
     </>
   );
 }
--- src/Components/BlueprintMesh.jsx
+++ src/Components/BlueprintMesh.jsx
@@ -1,13 +1,13 @@
 import React, { useRef } from 'react'
 import { useFrame } from 'react-three-fiber'
 
-function BluepringMesh({position}) {
+function BluepringMesh({position, args, color}) {
     const mesh = useRef(null)
     useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.005))
   return (
-    <mesh position={position} ref={mesh}>
-        <boxGeometry attach="geometry" args={[1,1,1]}/>
-        <meshStandardMaterial attach="material" color="pink"/>
+    <mesh position={position} ref={mesh} castShadow>
+        <boxGeometry attach="geometry" args={args}/>
+        <meshStandardMaterial attach="material" color={color}/>
     </mesh>
   )
 }
